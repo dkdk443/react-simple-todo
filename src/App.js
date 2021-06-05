4d5868579cca0510823013158b251c8221345072
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2021-06-05 18:28:23 +0900

    フィルター機能実装

--- src/App.js
+++ src/App.js
@@ -12,13 +12,11 @@ function App() {
     { id: 3, todo: 'タスク3', status: 2 },
     { id: 4, todo: 'タスク4', status: 3 }
   ]);
-  const [status, setStatus] = useState([
-    { id: 1, status: 'TODO'},
-    { id: 2, status: 'DOING' },
-    { id: 3, status: 'DONE' },
-  ])
+  const [status, setStatus] = useState('ALL');
+
   const [text, setText] = useState('');
-  let newId = todos.slice(-1)[0].id + 1;
+  // let newId = todos.slice(-1)[0].id + 1;
+  let newId = Math.random();
   const newTask = (text) => {
     setText(text);
   }
@@ -29,12 +27,29 @@ function App() {
       setTodos([...todos, { id: newId, todo: text, status: 1 }]);
     }
   }
+  const handleStatus = (status) => {
+    setStatus(status);
+    console.log(status);
+  }
+
+  const filterTodos = todos.filter(todo => {
+    if (status === 1) {
+      return todo.status === 1;
+    } else if (status === 2) {
+       return todo.status === 2;
+    } else if (status === 3) {
+      return todo.status === 3
+    } else {
+      return todo;
+    }
+  })
+
   return (
     <div className="App">
       <>
         <TodoForm addTask={addTask} newTask={newTask}/>
-        <TodoFilter />
-        <TodoList todos={todos} setTodos={setTodos} status={status} />
+        <TodoFilter handleStatus={handleStatus} />
+        <TodoList todos={filterTodos} setTodos={setTodos} status={status} />
       </>
     </div>
   );
--- src/components/TodoFilter.js
+++ src/components/TodoFilter.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-export default function TodoFilter() {
+export default function TodoFilter(props) {
   return (
     <div>
-      <div class="tabs">
+      <div className="tabs">
         <ul>
-          <li className="is-active"><a>TODO</a></li>
-          <li><a>DOING</a></li>
-          <li><a>DONE</a></li>
+          <li className="is-active" onClick={() => props.handleStatus('ALL')}><a>ALL</a></li>
+          <li onClick={() => props.handleStatus(1)}><a>TODO</a></li>
+          <li onClick={() => props.handleStatus(2)}><a>DOING</a></li>
+          <li onClick={() => props.handleStatus(3)}><a>DONE</a></li>
         </ul>
       </div>
     </div>
